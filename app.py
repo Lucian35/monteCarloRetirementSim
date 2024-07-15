65704d0a951b7435dc0a7f22629914bc4f374ccc
Lucian <147434586+Lucian35@users.noreply.github.com>
2024-07-14 22:35:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import math
 import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -11,30 +12,32 @@ with open('VFIAXHistoricalReturnsDaily.txt', 'r') as file:
 averageDailyReturn = sum([float(x) for x in VFIAXHistoricalReturnsDaily]) / len(VFIAXHistoricalReturnsDaily)
 averageYearlyReturn = (1 + averageDailyReturn) ** 365 - 1
 
-startingValue = st.number_input("Starting Value of Portfolio", value=38000, step=1000)
+startingValue = st.number_input("Starting Value of Portfolio", value=40000, step=1000)
 years = st.number_input("Years until death", value=66)
 numSimulations = st.number_input("Number of Simulations", value=100)
-yearsUntilRetirement = st.number_input("Years until retirement", value=10)
-initialCostOfLivingPerMonth = st.number_input("Cost of living (monthly)", value=3000, step=1000)
-initialCostOfLivingInRetirementPerMonth = st.number_input("Cost of living in retirement (monthly)", value=600, step=1000)
+initialCostOfLivingPerMonth = st.number_input("Cost of living (monthly)", value=4000, step=1000)
+initialCostOfLivingInRetirementPerMonth = st.number_input("Cost of living in retirement (monthly)", value=2000, step=1000)
 initialSalary = st.number_input("Salary", value=75000, step=1000)
 
-def monteCarloSimulation(startingValue, years, numSimulations, yearsUntilRetirement, initialCostOfLivingPerMonth, initialCostOfLivingInRetirementPerMonth, initialSalary):
-    days = years * 365
+def monteCarloSimulation(startingValue, years, numSimulations, initialCostOfLivingPerMonth, initialCostOfLivingInRetirementPerMonth, initialSalary):
     simulations = []
+    yearsItTookToRetire = []
     for _ in range(numSimulations):
         portfolioValuesOverTime = []
         portfolioValue = startingValue
         costOfLivingInRetirementPerMonth = initialCostOfLivingInRetirementPerMonth
         costOfLivingPerMonth = initialCostOfLivingPerMonth
         salary = initialSalary
+        days = years * 365
+        retired = False
         for day in range(days):
             portfolioValuesOverTime.append(portfolioValue)
             # Changes portfolio value by choosing a random return value of VFIAX since 2000
             #Vanguard 500 Index Fund Admiral Shares (VFIAX) has a 0.04% expense ratio
-            portfolioValue *= (1 + random.choice(VFIAXHistoricalReturnsDaily)) * (1 - 0.0004)
+            portfolioValue *= (1 + random.choice(VFIAXHistoricalReturnsDaily))
+            portfolioValue -= portfolioValue * 0.000001587
 
-            year = day // 365
+            year = day / 365
 
             #inflation is assumed to be 3% annually
             if day % 30 == 0:
@@ -44,50 +47,64 @@ def monteCarloSimulation(startingValue, years, numSimulations, yearsUntilRetirem
             if day % 365 == 0:
                 salary *= 1.03
 
-            if year <= yearsUntilRetirement:
+            if not retired:
+            #this is a conservative estimate
+                if costOfLivingInRetirementPerMonth * 1.03 * 12 / portfolioValue <= 0.03:
+                    retired = True
+                    yearsItTookToRetire.append(year)
+                    
+
+            if not retired:
                 # monthly
                 if day % 30 == 0:
                     portfolioValue -= costOfLivingPerMonth
-                # bi-monthly
-                if day % 15 == 0:
-                    portfolioValue += salary / 24
-            elif day % 30 == 0 and year > yearsUntilRetirement:
+                    portfolioValue += salary / 12
+            elif day % 30 == 0 and retired:
                 portfolioValue -= costOfLivingInRetirementPerMonth
 
         simulations.append(portfolioValuesOverTime)
+    st.write(f"Average years until retirement: {sum(yearsItTookToRetire) / len(yearsItTookToRetire)}")
+    st.write(f"Fastest retirement: {min(yearsItTookToRetire)}")
+    st.write(f"Slowest retirement: {max(yearsItTookToRetire)}")
     return simulations
 
-def nonMonteCarloSimulation(startingValue, years, yearsUntilRetirement, initialCostOfLivingPerMonth, initialCostOfLivingInRetirementPerMonth, initialSalary):
-    days = years * 365
+def nonMonteCarloSimulation(startingValue, years, initialCostOfLivingPerMonth, initialCostOfLivingInRetirementPerMonth, initialSalary):
     portfolioValuesOverTime = []
     portfolioValue = startingValue
     costOfLivingInRetirementPerMonth = initialCostOfLivingInRetirementPerMonth
     costOfLivingPerMonth = initialCostOfLivingPerMonth
     salary = initialSalary
+    retired = False
+    days = years * 365
     for day in range(days):
         portfolioValuesOverTime.append(portfolioValue)
-        # Changes portfolio value by increaseing by the average daily return of VFIAX since 2000
-        portfolioValue *= (1 + averageDailyReturn) * (1 - 0.0004)
 
-        year = day // 365
+        year = day / 365
 
         #inflation is assumed to be 3% annually
+        #Add a yearly increase option for myself later
         if day % 30 == 0:
             costOfLivingPerMonth *= ((1 + 0.03)**(1/12))
-        if day % 365 == 0:
-            costOfLivingInRetirementPerMonth *= 1.03
+            costOfLivingInRetirementPerMonth *= ((1 + 0.03)**(1/12))
         #salary increases by 3% annually
         if day % 365 == 0:
             salary *= 1.03
+        
+        #double check vangaurd fees
+        portfolioValue *= (1 + averageDailyReturn)
+        portfolioValue -= portfolioValue * 0.000001587
 
-        if year <= yearsUntilRetirement:
+        if not retired:
+            #this is a conservative estimate
+            if costOfLivingInRetirementPerMonth * 1.03 * 12 / portfolioValue <= 0.03:
+                retired = True
+
+        if not retired:
             # monthly
             if day % 30 == 0:
                 portfolioValue -= costOfLivingPerMonth
-            # bi-monthly
-            if day % 15 == 0:
-                portfolioValue += salary / 24
-        elif day % 30 == 0 and year > yearsUntilRetirement:
+                portfolioValue += salary / 12
+        elif retired and day % 30 == 0:
             portfolioValue -= costOfLivingInRetirementPerMonth
     return portfolioValuesOverTime
 
@@ -105,12 +122,15 @@ def calculateChanceOfSuccess(simulations):
     return successful / len(simulations) * 100
 
 if st.button("Run Simulation"):
-    simulations = monteCarloSimulation(startingValue, years, numSimulations, yearsUntilRetirement, initialCostOfLivingPerMonth, initialCostOfLivingInRetirementPerMonth, initialSalary)
+    simulations = monteCarloSimulation(startingValue, years, numSimulations, initialCostOfLivingPerMonth, initialCostOfLivingInRetirementPerMonth, initialSalary)
     st.write(f"Chance of Success: {calculateChanceOfSuccess(simulations)}%")
 
     # Change y-axis to display full numbers instead of scientific notation
     formatter = ticker.FuncFormatter(lambda x, p: '$' + format(int(x), ','))
     plt.gca().yaxis.set_major_formatter(formatter)
+
+    plt.ylim(0, 1500000 * 1.1)  # Add some padding
+    plt.xlim(0, years)
 
     # Generate x-axis values for years
     yearsForXAxis = [day/365 for day in range(0, len(simulations[0]))]
@@ -118,7 +138,7 @@ if st.button("Run Simulation"):
     # Plot the portfolio value over time
     for portfolioValuesOverTime in simulations:
         plt.plot(yearsForXAxis, portfolioValuesOverTime)
-    plt.plot(yearsForXAxis, nonMonteCarloSimulation(startingValue, years, yearsUntilRetirement, initialCostOfLivingPerMonth, initialCostOfLivingInRetirementPerMonth, initialSalary), color='black')
+    plt.plot(yearsForXAxis, nonMonteCarloSimulation(startingValue, years, initialCostOfLivingPerMonth, initialCostOfLivingInRetirementPerMonth, initialSalary), color='black')
     plt.xlabel('Years')
     plt.ylabel('Portfolio Value')
     plt.title('Portfolio Value Over Time')
